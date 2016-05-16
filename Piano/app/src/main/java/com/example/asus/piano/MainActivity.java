471331595b71f946db14fda1c45c42f545284629
puntonugroho <punugroho@gmail.com>
2016-05-16 10:18:30 +0700

    oop-project
    oop-project

---
+++ Piano/app/src/main/java/com/example/asus/piano/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.asus.piano;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ Piano/app/src/main/java/com/example/asus/piano/Piano.java
@@ -0,0 +1,113 @@
+package com.example.asus.piano;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.LinearLayout;
+
+/**
+ * Created by ASUS on 5/15/2016.
+ */
+public class Piano extends View {
+    public Piano(Context context){
+        super(context);
+    }
+    public Piano(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+    public Piano(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+
+    Bitmap whiteKey, blackKey;
+    Paint paint =new Paint();
+
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec){
+        int desireWidth=1000;
+        int desireHeight=300;
+
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int width;
+        int height;
+
+        if (widthMode == MeasureSpec.EXACTLY){
+            width = widthSize;
+        }
+        else if (widthMode == MeasureSpec.AT_MOST){
+            width = Math.min(desireWidth,widthSize);
+        }
+        else{
+            width = desireWidth;
+        }
+
+        if (heightMode==MeasureSpec.EXACTLY){
+            height = heightSize;
+        }
+        else if (heightMode == MeasureSpec.AT_MOST){
+            height = Math.min(desireHeight,heightSize);
+        }
+        else {
+            height = desireHeight;
+        }
+        setMeasuredDimension(width,height);
+    }
+
+    public void draw(Canvas canvas){
+
+        if (whiteKey == null){
+            whiteKey = BitmapFactory.decodeResource(getResources(), R.drawable.pianokeyswhite);
+        }
+        if (blackKey == null){
+            blackKey = BitmapFactory.decodeResource(getResources(),R.drawable.pianokeysblack);
+        }
+        int keys = 13;
+        int i = 0;
+        for (; i < keys ;i++) {
+            canvas.drawBitmap(whiteKey, i * whiteKey.getWidth(), 0, paint);
+            if (i==1){
+                canvas.drawBitmap(blackKey, i * blackKey.getWidth() + blackKey.getWidth() * 0.3f, 0, paint);
+            }
+
+            else if (i==2){
+                canvas.drawBitmap(blackKey, i * blackKey.getWidth() + blackKey.getWidth() * 0.9f, 0, paint);
+            }
+
+            else if (i==4){
+                canvas.drawBitmap(blackKey, i * blackKey.getWidth() + blackKey.getWidth() * 2.2f, 0, paint);
+            }
+
+            else if (i==5){
+                canvas.drawBitmap(blackKey, i * blackKey.getWidth() + blackKey.getWidth() * 2.9f, 0, paint);
+            }
+
+            else if (i==6){
+                canvas.drawBitmap(blackKey, i * blackKey.getWidth() + blackKey.getWidth() * 3.6f, 0, paint);
+            }
+
+            else if (i==8){
+                canvas.drawBitmap(blackKey, i * blackKey.getWidth() + blackKey.getWidth() * 4.9f, 0, paint);
+            }
+
+            else if (i==9){
+                canvas.drawBitmap(blackKey, i * blackKey.getWidth() + blackKey.getWidth() * 5.6f, 0, paint);
+            }
+
+            else if (i==11){
+                canvas.drawBitmap(blackKey, i * blackKey.getWidth() + blackKey.getWidth() * 6.9f, 0, paint);
+            }
+
+            else if (i==12){
+                canvas.drawBitmap(blackKey, i * blackKey.getWidth() + blackKey.getWidth() * 7.6f, 0, paint);
+            }
+            }
+        }
+    };
